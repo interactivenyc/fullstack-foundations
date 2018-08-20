6c78e57776c5b0de2e03a699b121479cd5416ea0
Steve Warren <steve@interactivenyc.com>
2018-08-20 09:23:50 -0400

    new module in progress

--- Part 03 - TestFirst-Review-V2/00_pig_latin/pig_latin.js
+++ Part 03 - TestFirst-Review-V2/00_pig_latin/pig_latin.js
@@ -1,3 +1,26 @@
+function pigify(phrase) {
+    let wordArray = phrase.split(' ');
+    let vowels = 'aeiou';
+    // console.log('wordArray', wordArray);
+    
 
+    for (let i=0; i<wordArray.length; i++) {
+        let word = wordArray[i];
+        // console.log('word', word);
+        
+        for (let j = 0; j < word.length; j++) {
+            if (vowels.includes(word[j])) {
+                // console.log('word', j, word.substring(0, j), ':', word.substring(j, word.length));
+                let pigWord = word.substring(j, word.length) + word.substring(0, j) + 'ay';
+                wordArray[i] = pigWord;
+                break;
+            }
+        }
+    }
+    console.log('pigify', wordArray.join(' '));
+    return wordArray.join(' ');
+}
+
+pigify('when the lights go out in the city');
 
 
