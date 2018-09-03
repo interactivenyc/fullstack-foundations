b515297100a90a40fe5090370f6c283031540684
Steve Warren <steve@interactivenyc.com>
2018-09-03 12:42:29 -0400

    starting guessing game

--- HTML-CSS/guessing-game/js/guessing-game.js
+++ HTML-CSS/guessing-game/js/guessing-game.js
@@ -7,14 +7,58 @@ In this file, you will also include the event listeners that are needed to inter
 a user clicks a button or adds a guess to the input field.
 
 */
+let winningNumber;
+let guesses = [];
+let inputField;
+let prompt;
+
+function initialize() {
+    console.log('initialize');
+    
+    inputField = document.getElementById('inputField');
+    inputField.addEventListener('keydown', onKeyboardEvent);
+
+    prompt = document.getElementById('prompt');
+    prompt.innerHTML = 'Enter a number between 0 and 100';
+
+    reset();
+}
+
+function onKeyboardEvent(e) {
+    if (e.key === 'Enter') {
+        console.log('onKeyboardEvent', e);
+        guess();
+    }
+}
+
+function guess() {
+    let currentGuess = inputField.value;
+    inputField.value = '';
+    guesses.push(currentGuess);
+    console.log('guess', currentGuess, guesses);
+
+    if (currentGuess === winningNumber.toString()) {        
+        prompt.innerHTML = 'You WIN!!!';
+    } else if (guesses.length >= 5) {
+        prompt.innerHTML = 'You LOSE!!!';
+    } else {
+        currentGuess < winningNumber ? prompt.innerHTML = 'Too Low' : prompt.innerHTML = 'Too High';
+    }
+    
+}
+
+function reset() {
+    console.log('reset');
+    guesses = [];
+    generateWinningNumber();
+}
 
 function generateWinningNumber() {
     console.log('generateWinningNumber');
 
-    let rnd = Math.floor(Math.random() * 100);
+    winningNumber = Math.floor(Math.random() * 100);
 
-    console.log('rnd', rnd);
+    console.log('winningNumber', winningNumber);
 
-    
-    
 }
+
